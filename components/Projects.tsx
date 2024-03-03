eba756ba3901de6f855137a03b631f7f4d612559
Jan Bojko <honzabojko@seznam.cz>
2024-03-03 18:36:01 +0100

    Nav, projects scroll fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,25 +8,25 @@ import { useSectionInView } from "@/lib/hooks";
 import { useLanguage } from "@/context/language-context";
 
 const Projects = () => {
-  const { ref } = useSectionInView("Projects", 0.5);
-  const { language } = useLanguage();
+	const { ref } = useSectionInView("Projects", 0.25);
+	const { language } = useLanguage();
 
-  let ProjectsData = language === "english" ? projectsData : projectsDataCz;
+	let ProjectsData = language === "english" ? projectsData : projectsDataCz;
 
-  return (
-    <section id='projects' className='scroll-mt-28 mb-28' ref={ref}>
-      <SectionHeading>
-        {language === "english" ? <>My Projects</> : <>Moje Projekty</>}
-      </SectionHeading>
-      <div className='flex flex-col'>
-        {ProjectsData.map((project, index) => (
-          <React.Fragment key={index}>
-            <Project {...project} />
-          </React.Fragment>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section id="projects" className="scroll-mt-28 mb-28" ref={ref}>
+			<SectionHeading>
+				{language === "english" ? <>My Projects</> : <>Moje Projekty</>}
+			</SectionHeading>
+			<div className="flex flex-col">
+				{ProjectsData.map((project, index) => (
+					<React.Fragment key={index}>
+						<Project {...project} />
+					</React.Fragment>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
