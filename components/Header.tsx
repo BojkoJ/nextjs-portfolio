76fc086ecf3e4396dde5a9861a7f77e64207af7a
Jan Bojko <honzabojko@seznam.cz>
2025-10-23 12:32:58 +0200

    fix of navigation for mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
     const Links = language === "english" ? links : linksCz;
 
     let motionDivClassName =
-        "fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75";
+        "fixed top-0 left-1/2 h-[6rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75";
 
     if (language === "czech") {
         motionDivClassName += " sm:w-[46rem] ";
